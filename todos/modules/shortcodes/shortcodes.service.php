b84c20d74cc1bfce8139f802aaebf37da902865d
Pavel Khatkevich <pavel.khatkevich2000@gmail.com>
2024-07-22 17:41:53 +0300

    added shortcodes and custom theme

--- todos/modules/shortcodes/shortcodes.service.php
+++ todos/modules/shortcodes/shortcodes.service.php
@@ -5,8 +5,34 @@ namespace Todos\Modules;
 use Todos\Instances\WithLogger;
 
 class ShortcodeService extends WithLogger {
-    public function todos(): void
+    public function todos(mixed $atts): string
     {
-        echo '1';
+        global $wpdb;
+
+        $args = gettype($atts) === 'array' ? $atts : [];
+        $table_name = $wpdb->prefix . 'todos';
+        [
+            'limit' => $count,
+            'completed' => $completed,
+        ] = array_merge([
+            'limit' => 5,
+            'completed' => null,
+        ], $args);
+
+        $completed_sql = '';
+        if ($completed === 'true') {
+            $completed_sql = " AND completed = true";
+        }
+        if ($completed === 'false') {
+            $completed_sql = " AND completed = false";
+        }
+
+        $result = $wpdb->get_results($wpdb->prepare("
+            SELECT * FROM $table_name WHERE 1=1
+            $completed_sql
+            LIMIT $count
+        "));
+
+        return json_encode($result ?? []);
     }
 }
